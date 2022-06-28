367ad82b30d750b856bf29460a3d392b355ff741
Postdie <fernandezdiego845@gmail.com>
2022-06-28 03:44:51 -0500

    commit 13

---
+++ Codigo_python/Bucles.py
@@ -0,0 +1,68 @@
+contador = 0
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 1
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 2
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 3
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 4
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 5
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 6
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 7
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 8
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 9
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 10
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 11
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 12
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 13
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 14
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 15
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 16
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 17
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 18
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 19
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 20
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 21
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 22
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 23
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 24
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 25
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 26
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 27
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 28
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 29
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 30
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 31
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 32
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
+contador = 33
+print("2 elavado a " + str(contador)+ " es igual: " + str(2**contador))
